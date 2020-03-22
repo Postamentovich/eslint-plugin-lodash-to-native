ef7116375de0db5c7d523fbd15590db5d0df16b2
postamentovich <postamentovich@gmail.com>
2020-03-22 21:45:47 +0300

    add comments

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -17,10 +17,17 @@ const RuleTester = require("eslint").RuleTester;
 
 var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
 ruleTester.run("map", rule, {
-    valid: ["_.map({}, () => {})"],
+    valid: [
+        /** Если объект объявлен явно, код считается валидным */
+        "_.map({}, () => {})",
+        /** Если _ был переопределён, то правило не должно срабатывать после переопределения */
+        "_ = {map: () => []}; _.map([0], () => {})"
+    ],
     invalid: [
         {
+            /** Если при вызове явно указан литерал массива, то фикс должен генерировать код без проверки, что параметр - это массив */
             code: "_.map([0], () => {})",
+            output: "[0].map(() => {})",
             errors: [
                 {
                     message: "Prefer native Array.map method"
@@ -28,7 +35,18 @@ ruleTester.run("map", rule, {
             ]
         },
         {
-            code: "let a = [0]; _.map(a, () => {})",
+            code: "_.map(collection, () => {})",
+            output:
+                "Array.isArray(collection) ? collection.map(() => {}) : _.map(collection, () => {})",
+            errors: [
+                {
+                    message: "Prefer native Array.map method"
+                }
+            ]
+        },
+        {
+            code: "const _ = require('lodash'); _.map([0], () => {})",
+            output: "const _ = require('lodash'); [0].map(() => {})",
             errors: [
                 {
                     message: "Prefer native Array.map method"
