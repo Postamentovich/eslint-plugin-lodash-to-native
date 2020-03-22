7c315a07958400db1e4328aa88aea61f41492551
postamentovich <postamentovich@gmail.com>
2020-03-22 18:42:17 +0300

    update rule

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -8,26 +8,32 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/map"),
-  RuleTester = require("eslint").RuleTester;
+const rule = require("../../../lib/rules/map");
+const RuleTester = require("eslint").RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
 ruleTester.run("map", rule, {
-  valid: ["_.map({}, {} => {}))"],
-
-  invalid: [
-    {
-      code: "_.map([0], () => {})))",
-      errors: [
+    valid: ["_.map({}, () => {})"],
+    invalid: [
         {
-          message: "Fill me in.",
-          type: "Me too"
+            code: "_.map([0], () => {})",
+            errors: [
+                {
+                    message: "Prefer native Array.map method"
+                }
+            ]
+        },
+        {
+            code: "let a = [0]; _.map(a, () => {})",
+            errors: [
+                {
+                    message: "Prefer native Array.map method"
+                }
+            ]
         }
-      ]
-    }
-  ]
+    ]
 });
